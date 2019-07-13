a15531863e79fed306898e1ef8359f6f6decf759
俞佳兴 <alberty@dynamic.ipv6.sjtu.edu.cn>
2019-07-13 12:13:16 +0800

    Initial Commit

---
+++ Secured2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Secured2048
+//
+//  Created by Mikeboge on 2019/7/13.
+//  Copyright © 2019年 Mikeboge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
