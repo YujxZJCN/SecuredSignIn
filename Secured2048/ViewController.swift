ad8ff2133477b85c986bb9b1aa939eb8bf96f86e
俞佳兴 <alberty@mikeboges-macbook-pro.local>
2019-07-14 09:28:43 +0800

    Initial Version.

--- Secured2048/ViewController.swift
+++ Secured2048/ViewController.swift
@@ -7,14 +7,290 @@
 //
 
 import UIKit
+import LocalAuthentication
+import SkyFloatingLabelTextField
+import Alamofire
+import Kanna
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var logoName: UILabel!
+    @IBOutlet weak var hiddenEmailLabel: UILabel!
+    var url = "http://178.128.26.180:8787"
+    var hiddenEmail = "securedemail@secure.com" {
+        didSet {
+            hiddenEmailLabel.text = "Forward To: " + hiddenEmail
+        }
+    }
+    let nameTextField = SkyFloatingLabelTextField(frame: CGRect(x: 40, y: 40, width: 200, height: 45))
+    let emailTextField = SkyFloatingLabelTextField(frame: CGRect(x: 40, y: 40, width: 200, height: 45))
+    let darkGreyColor = #colorLiteral(red: 0.5654503107, green: 0.8293422461, blue: 0.8113552928, alpha: 1)
+    let lightGreyColor = #colorLiteral(red: 0.5654503107, green: 0.8293422461, blue: 0.8113552928, alpha: 1)
+    let overcastBlueColor = UIColor(red: 0, green: 187/255, blue: 204/255, alpha: 1.0)
+    @IBOutlet weak var formerEmailLabel: UILabel!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameTextField.resignFirstResponder()
+        emailTextField.resignFirstResponder()
+        UIView.animate(withDuration: 0.3, animations: {
+            self.logo.transform = .identity
+            self.logoName.transform = .identity
+            self.nameTextField.transform = .identity
+            self.emailTextField.transform = .identity
+        }, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let moveUpTransform = CGAffineTransform.init(translationX: 0, y: -90)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.logo.transform = moveUpTransform
+            self.logoName.transform = moveUpTransform
+            self.nameTextField.transform = moveUpTransform
+            self.emailTextField.transform = moveUpTransform
+        }, completion: nil)
+        
+    }
+    
+    @IBOutlet weak var signInButton: UIButton! {
+        didSet {
+            signInButton.clipsToBounds = true
+            signInButton.layer.cornerRadius = 10
+        }
+    }
+    let blurEffect = UIBlurEffect(style: .dark)
+    lazy var blurEffectView = UIVisualEffectView(effect: blurEffect)
+    
+    @IBAction func SignIn(_ sender: UIButton) {
+        nameLabel.text = nameTextField.text
+        formerEmailLabel.text = emailTextField.text
+        blurEffectView.frame = view.bounds
+        self.view.addSubview(blurEffectView)
+        self.view.bringSubviewToFront(signInView)
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 2, initialSpringVelocity: 1.5, options: [], animations: {
+            self.signInView.transform = .identity
+            self.signInView.alpha = 1.0
+        }, completion: nil)
+    }
+    
+    @IBOutlet weak var signInView: UIView!
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        let moveDownTransform = CGAffineTransform.init(translationX: 0, y: 380)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.signInView.transform = moveDownTransform
+            self.signInView.alpha = 0.0
+        }, completion: { completion in
+            self.blurEffectView.removeFromSuperview()
+        })
+    }
+    
+    @IBOutlet weak var nameBox: UIButton!
+    @IBOutlet weak var shareEmailBox: UIButton!
+    @IBOutlet weak var hideMyEmailButton: UIButton!
+    
+    var share = false {
+        didSet {
+            if share {
+                hide = false
+            }
+            if share {
+                shareEmailBox.setImage(UIImage(named: "check"), for: .normal)
+            } else {
+                shareEmailBox.setImage(UIImage(named: "circle"), for: .normal)
+            }
+        }
+    }
+    var hide = false {
+        didSet {
+            if hide {
+                share = false
+            }
+            if hide {
+                hideMyEmailButton.setImage(UIImage(named: "check"), for: .normal)
+            } else {
+                hideMyEmailButton.setImage(UIImage(named: "circle"), for: .normal)
+            }
+        }
+    }
+    
+    @IBOutlet weak var continueButton: UIButton! {
+        didSet {
+            continueButton.clipsToBounds = true
+            continueButton.layer.cornerRadius = 10
+        }
+    }
+    @IBAction func continueAction(_ sender: UIButton) {
+        authenticateWithBiometric()
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        nameTextField.delegate = self
+        nameTextField.placeholder = "Name"
+        nameTextField.title = "Username"
+        nameTextField.tintColor = overcastBlueColor
+        nameTextField.textColor = darkGreyColor
+        nameTextField.lineColor = lightGreyColor
+        nameTextField.selectedTitleColor = overcastBlueColor
+        nameTextField.selectedLineColor = overcastBlueColor
+        nameTextField.lineHeight = 1.0
+        nameTextField.selectedLineHeight = 2.0
+        nameTextField.center = self.view.center
+        nameTextField.textAlignment = .center
+        self.view.addSubview(nameTextField)
+        
+        emailTextField.delegate = self
+        emailTextField.placeholder = "Email"
+        emailTextField.title = "Email"
+        emailTextField.tintColor = overcastBlueColor
+        emailTextField.textColor = darkGreyColor
+        emailTextField.lineColor = lightGreyColor
+        emailTextField.selectedTitleColor = overcastBlueColor
+        emailTextField.selectedLineColor = overcastBlueColor
+        emailTextField.lineHeight = 1.0
+        emailTextField.selectedLineHeight = 2.0
+        emailTextField.center = CGPoint(x: self.view.center.x, y: self.view.center.y + 80)
+        emailTextField.textAlignment = .center
+        self.view.addSubview(emailTextField)
+        
+        signInButton.isEnabled = false
+        hide = true
+        hiddenEmail = "securedemail@secure.com"
+        signInButton.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
 
+    }
+    @IBOutlet weak var ORLabel: UILabel!
+    @IBOutlet weak var withDefaultLabel: UILabel!
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if nameTextField.text == "" || emailTextField.text == "" {
+            signInButton.isEnabled = false
+            signInButton.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        } else {
+            signInButton.isEnabled = true
+            signInButton.backgroundColor = #colorLiteral(red: 0.5654503107, green: 0.8293422461, blue: 0.8113552928, alpha: 1)
+        }
+        return true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let moveDownTransform = CGAffineTransform.init(translationX: 0, y: 380)
+        self.signInView.transform = moveDownTransform
+        self.signInView.alpha = 0.0
+        if nameTextField.text != "" {
+            signInButton.alpha = 0.0
+            ORLabel.alpha = 0.0
+            withDefaultLabel.alpha = 0.0
+            nameTextField.lineColor = .darkGray
+            emailTextField.lineColor = .darkGray
+            nameTextField.isEnabled = false
+            if hide {
+                emailTextField.text = hiddenEmail
+            }
+            emailTextField.isEnabled = false
+        }
+    }
+    
+    @IBAction func clearName(_ sender: UIButton) {
+        nameLabel.text = ""
+    }
+    @IBAction func shareMyEmailButtonClicked(_ sender: UIButton) {
+        share = !share
+    }
+    
+    
+    @IBAction func hideMyEmailButtonClicked(_ sender: UIButton) {
+        hide = !hide
+    }
+    
+    func authenticateWithBiometric() {
+        // Get the local authentication context.
+        let localAuthContext = LAContext()
+        let reasonText = "Authentication is required to sign in ToDoList"
+        var authError: NSError?
+        
+        if !localAuthContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+            
+            if let error = authError {
+                print(error.localizedDescription)
+            }
+            
+            // Display the login dialog when Touch ID is not available (e.g. in simulator)
+            print("login with Touch ID is not available")
+            return
+        }
+        
+        // Perform the Touch ID authentication
+        localAuthContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonText, reply: { (success: Bool, error: Error?) -> Void in
+            
+            // Failure workflow
+            if !success {
+                if let error = error {
+                    switch error {
+                    case LAError.authenticationFailed:
+                        print("Authentication failed")
+                    case LAError.passcodeNotSet:
+                        print("Passcode not set")
+                    case LAError.systemCancel:
+                        print("Authentication was canceled by system")
+                    case LAError.userCancel:
+                        print("Authentication was canceled by the user")
+                    case LAError.biometryNotEnrolled:
+                        print("Authentication could not start because you haven't enrolled either Touch ID or Face ID on your device.")
+                    case LAError.biometryNotAvailable:
+                        print("Authentication could not start because Touch ID / Face ID is not available.")
+                    case LAError.userFallback:
+                        print("User tapped the fallback button (Enter Password).")
+                    default:
+                        print(error.localizedDescription)
+                    }
+                }
+                
+                // Fallback to password authentication
+//                OperationQueue.main.addOperation({
+//                    self.showLoginDialog()
+//                })
+            } else {
+                
+                // Success workflow
+                
+                print("Successfully authenticated")
+                OperationQueue.main.addOperation({
+                    
+                    var para: [String : Any] = ["username_login" : "",
+                                                "password_login" : "",
+                    ]
+                    para["username_login"] = self.nameTextField.text
+                    if self.share {
+                        para["password_login"] = self.formerEmailLabel.text
+                    } else {
+                        para["password_login"] = self.hiddenEmail
+                    }
+                    
+                    Alamofire.request(self.url, method: .post, parameters: para).responseData { (response) in
+                        if let html = response.result.value, let doc = try? HTML(html: html, encoding: .utf8) {
+                            print(doc.content)
+                        }
+                    }
+                    
+//                    Alamofire.request(self.url, method: .get).responseData { (response) in
+//                        if let html = response.result.value, let doc = try? HTML(html: html, encoding: .utf8) {
+//                            print(doc.content)
+//                        }
+//                    }
+                    let game = NumberTileGameViewController(dimension: 4, threshold: 2048)
+                    self.blurEffectView.removeFromSuperview()
+                    let moveDownTransform = CGAffineTransform.init(translationX: 0, y: 380)
+                    self.signInView.transform = moveDownTransform
+                    self.signInView.alpha = 0.0
+                    self.present(game, animated: true, completion: nil)
+                })
+            }
+            
+        })
+    }
 
 }
 
